dad52092274df69cb08909bc326556df7b0e203d
grayson-orr <graysono@op.ac.nz>
2021-01-23 09:35:30 +1300

    Update res files

---
+++ 12-room-database/restaurant-customer-feedback/app/src/main/java/op/mobile/dev/restaurant/customer/feedback/MainActivity.kt
@@ -0,0 +1,17 @@
+package op.mobile.dev.restaurant.customer.feedback
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- 15-google-maps/README.md
+++ 15-google-maps/README.md
@@ -1,4 +1,4 @@
-# **Repository**
+# **Google Maps**
 
 ## Practical
 The practical for this topic is available [here]().
