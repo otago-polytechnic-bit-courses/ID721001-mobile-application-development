15d3bd8dea473e12928f97ccdc5432885310fbd7
grayson-orr <graysono@op.ac.nz>
2021-03-16 07:18:45 +1300

    Update

--- 06-fragments/06-bottom-navigation/app/src/main/java/op/mobile/app/dev/bottom/navigation/MainActivity.kt
+++ 06-fragments/06-bottom-navigation/app/src/main/java/op/mobile/app/dev/bottom/navigation/MainActivity.kt
@@ -3,6 +3,7 @@ package op.mobile.app.dev.bottom.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
