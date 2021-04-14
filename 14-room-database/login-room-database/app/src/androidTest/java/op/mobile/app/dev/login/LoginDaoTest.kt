7f5821d9f772bef4b7199d47e0749656bf6da0ec
grayson-orr <graysono@op.ac.nz>
2021-04-15 05:51:56 +1200

    Update

--- 14-room-database/login-room-database/app/src/androidTest/java/op/mobile/app/dev/login/LoginDaoTest.kt
+++ 14-room-database/login-room-database/app/src/androidTest/java/op/mobile/app/dev/login/LoginDaoTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import op.mobile.app.dev.login.database.ILoginDao
 import op.mobile.app.dev.login.database.LoginDb
@@ -40,11 +41,10 @@ class LoginDaoTest {
 
     @Test
     @Throws(IOException::class)
-    fun insertLoginDetailAndGetSecondLoginDetail() = runBlocking {
-        var login = Login("john.doe", "P@ssw0rd123")
+    fun insertLoginDetailAndGetUsername() = runBlocking {
+        val login = Login("john.doe", "P@ssw0rd123")
         dao.insert(login)
-        login = Login("jane.doe", "P@ssw0rd123")
-        dao.insert(login)
-        val allLoginDetails = dao.getAll()
+        val allLoginDetails = dao.getAll().first()
+        assertEquals(allLoginDetails[0].username, login.username)
     }
 }
