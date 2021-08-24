4f04fe3177143190fee13e26b7769453ae6ddf64
grayson-orr <graysono@op.ac.nz>
2021-08-24 15:13:32 +1200

    Update

---
+++ code-resources/api-retrofit/app/src/main/java/op/mobile/app/dev/api/MainActivity.kt
@@ -0,0 +1,24 @@
+package op.mobile.app.dev.api
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_github_jobs
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
