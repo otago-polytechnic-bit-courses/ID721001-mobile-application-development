c75d6d8b7c5d208cff9f1571390871b3daa784f2
Grayson Orr <grayson.orr@op.ac.nz>
2022-03-28 12:37:40 +1300

    Add files via upload

---
+++ resources/quiz-example/app/src/main/java/op/mobile/app/dev/graysono/travelling/MainActivity.kt
@@ -0,0 +1,24 @@
+package op.mobile.app.dev.graysono.travelling
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+}
