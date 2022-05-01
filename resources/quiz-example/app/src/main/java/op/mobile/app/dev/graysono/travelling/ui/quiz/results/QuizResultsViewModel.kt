8c3bfe762349e461a574d1899af8eae80dc15482
Grayson-Orr <grayson.orr@op.ac.nz>
2022-05-02 01:18:36 +1200

    chore: add google map example application

--- resources/quiz-example/app/src/main/java/op/mobile/app/dev/graysono/travelling/ui/quiz/results/QuizResultsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package op.mobile.app.dev.graysono.travelling.ui.quiz.results
-
-import androidx.lifecycle.ViewModel
-
-class QuizResultsViewModel(_score: Int) : ViewModel() {
-    var score: Int = _score
-}
